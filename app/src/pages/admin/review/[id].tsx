96346d92b6476a64e23f7c3ba5de4d5a9eccd8a1
fervillalbag <21fervillalbag@gmail.com>
2022-02-01 14:21:56 -0300

    feat: admin/review and admin/review/id styles

--- app/src/pages/admin/review/[id].tsx
+++ app/src/pages/admin/review/[id].tsx
@@ -8,6 +8,7 @@ import client from "@/config/apollo";
 import { GET_REVIEW_HOME_ITEM } from "@/graphql/queries/reviewHome";
 import { useMutation } from "@apollo/client";
 import { UPDATE_REVIEW_HOME_ITEM } from "@/graphql/mutation/reviewHome";
+import { BsFillArrowLeftCircleFill, BsTrash } from "react-icons/bs";
 
 const ReviewItemId = () => {
   const router = useRouter();
@@ -22,6 +23,7 @@ const ReviewItemId = () => {
     status: false,
   });
 
+  const inputFileRef = React.useRef<any>(null);
   const [updateReviewHome] = useMutation(UPDATE_REVIEW_HOME_ITEM);
 
   React.useEffect(() => {
@@ -43,6 +45,10 @@ const ReviewItemId = () => {
   const newDescription = {
     id: uuidv4(),
     text: "",
+  };
+
+  const handleChangeImage = () => {
+    inputFileRef.current.click();
   };
 
   const handleAddInputDescription = () => {
@@ -122,61 +128,115 @@ const ReviewItemId = () => {
   if (!data || !descriptionArray) return <Loading />;
 
   return (
-    <div className="p-4">
-      <input
-        className="w-11/12 border p-2"
-        type="text"
-        value={data?.name}
-        onChange={(e) => setData({ ...data, name: e.target.value })}
-      />
-
-      <div className="py-4">
-        <input type="file" onChange={handleHeaderFileChange} />
-        <div className="pt-4">
-          {!showHeaderImage ? (
-            <img src={data?.avatar} width={100} alt="" />
-          ) : (
-            <img src={showHeaderImage} width={100} alt="" />
-          )}
-        </div>
+    <div className="flex items-start">
+      <div className="px-12 pt-10">
+        <button
+          className="border border-slate-300 rounded flex items-center justify-center px-3 py-2 text-slate-500 mb-8 w-32"
+          onClick={() => router.back()}
+        >
+          <span className="mr-2">
+            <BsFillArrowLeftCircleFill />
+          </span>
+          <span>Back</span>
+        </button>
       </div>
 
-      <div className="py-4">
-        {descriptionArray.map((description: any, index: number) => (
-          <div key={description.id} className="flex">
-            <textarea
-              className="block w-11/12 border p-2"
-              value={description.text}
-              onChange={(e) => {
-                const text = e.target.value;
-                setDescriptionArray((currentDescription: any) =>
-                  produce(currentDescription, (v: any) => {
-                    v[index].text = text;
-                  })
-                );
-              }}
-            ></textarea>
+      <div className="p-10 w-full h-screen overflow-y-auto no-scrollbar">
+        <h1 className="text-3xl text-slate-600">
+          Review of: <span className="text-slate-400">{data?.name}</span>
+        </h1>
+
+        {!data || !descriptionArray ? (
+          <Loading />
+        ) : (
+          <section className="py-8">
+            <div>
+              <span className="block text-sm mb-2 text-slate-500">Title:</span>
+              <input
+                className="w-full block border border-slate-300 rounded px-3 py-2 focus:border-slate-500 focus:outline-0 transition-all duration-300"
+                type="text"
+                value={data?.name}
+                onChange={(e) => setData({ ...data, name: e.target.value })}
+              />
+            </div>
+
+            <div className="py-4">
+              <button
+                className="border border-slate-300 rounded block px-3 py-2 text-slate-500 mb-2"
+                onClick={handleChangeImage}
+              >
+                Change image
+              </button>
+              <input
+                type="file"
+                ref={inputFileRef}
+                onChange={handleHeaderFileChange}
+                className="hidden"
+              />
+              <div className="pt-4">
+                {!showHeaderImage ? (
+                  <img src={data?.avatar} width={100} alt="" />
+                ) : (
+                  <img src={showHeaderImage} width={100} alt="" />
+                )}
+              </div>
+            </div>
+
+            <div>
+              <h3 className="text-slate-600 mt-4">Description:</h3>
+
+              {descriptionArray.length === 0 ? (
+                <span className="block py-4 text-slate-900">
+                  No description available
+                </span>
+              ) : (
+                descriptionArray.map((description: any, index: number) => (
+                  <div key={description.id} className="flex py-4">
+                    <textarea
+                      className="w-full block border border-slate-300 rounded px-3 py-2 focus:border-slate-500 focus:outline-0 transition-all duration-300 resize-none h-32"
+                      value={description.text}
+                      onChange={(e) => {
+                        const text = e.target.value;
+                        setDescriptionArray((currentDescription: any) =>
+                          produce(currentDescription, (v: any) => {
+                            v[index].text = text;
+                          })
+                        );
+                      }}
+                    ></textarea>
+                    <button
+                      className="block p-2 text-2xl px-5 text-red-500 bg-slate-100 ml-4 rounded"
+                      onClick={() =>
+                        handleDeleteInputDescription(description.id)
+                      }
+                    >
+                      <BsTrash />
+                    </button>
+                  </div>
+                ))
+              )}
+            </div>
+
             <button
-              className="block border p-2"
-              onClick={() => handleDeleteInputDescription(description.id)}
+              className="border border-slate-300 rounded block px-3 py-2 text-slate-500 mb-8"
+              onClick={handleAddInputDescription}
             >
-              delete
+              Add input description
             </button>
-          </div>
-        ))}
+
+            <div className="border border-slate-200"></div>
+
+            <button
+              className="bg-slate-700 text-white rounded block px-8 text-lg py-2 mt-8"
+              onClick={handleUpdate}
+            >
+              Update
+            </button>
+
+            {error.status && <span className="block">{error.type}</span>}
+          </section>
+        )}
       </div>
-
-      <button
-        className="block p-2 border mb-4"
-        onClick={handleAddInputDescription}
-      >
-        agregar campo
-      </button>
-      <button className="block p-2 border" onClick={handleUpdate}>
-        Actualizar
-      </button>
-
-      {error.status && <span className="block">{error.type}</span>}
     </div>
   );
 };
--- app/src/pages/admin/review/index.tsx
+++ app/src/pages/admin/review/index.tsx
@@ -11,13 +11,15 @@ import { GET_REVIEW_INFO } from "@/graphql/queries/reviewInfo";
 import { UPDATE_REVIEW_INFO } from "@/graphql/mutation/reviewInfo";
 import { GET_REVIEW_HOME } from "@/graphql/queries/reviewHome";
 import { DELETE_REVIEW_HOME_ITEM } from "@/graphql/mutation/reviewHome";
+import { BsTrash } from "react-icons/bs";
+import NavbarDashboard from "@/components/admin/Navbar";
 
 const ReviewInfoAdmin = () => {
   const router = useRouter();
 
   const [data, setData] = React.useState<any>(null);
   const [descriptionArray, setDescriptionArray] = React.useState<any>([]);
-  const [dataItems, setDataItems] = React.useState<any>(null);
+  const [dataItems, setDataItems] = React.useState<any>([]);
   const [showModal, setShowModal] = React.useState<boolean>(false);
   const [itemDelete, setItemDelete] = React.useState<string>("");
   const [error, setError] = React.useState<any>({
@@ -112,119 +114,149 @@ const ReviewInfoAdmin = () => {
     });
   };
 
-  if (!data || !descriptionArray || !dataItems) return <Loading />;
-
   return (
-    <div className="p-4">
-      <div>
-        <input
-          type="text"
-          value={data?.title}
-          className="border w-11/12 p-2"
-          onChange={(e) => setData({ ...data, title: e.target.value })}
-        />
-      </div>
+    <div className="flex items-start">
+      <NavbarDashboard />
 
-      <div className="py-4">
-        {descriptionArray.length === 0 ? (
-          <span className="block py-4 text-slate-900">
-            No description available
-          </span>
+      <div className="p-10 w-full h-screen overflow-y-auto no-scrollbar">
+        <h1 className="text-3xl text-slate-600">Review Section</h1>
+
+        {!data || !descriptionArray ? (
+          <Loading />
         ) : (
-          descriptionArray.map((description: any, index: number) => (
-            <div key={description.id} className="flex py-4">
-              <textarea
-                className="border w-11/12 p-2"
-                value={description.text}
-                onChange={(e) => {
-                  const text = e.target.value;
-                  setDescriptionArray((currentDescription: any) =>
-                    produce(currentDescription, (v: any) => {
-                      v[index].text = text;
-                    })
-                  );
-                }}
-              ></textarea>
-              <button
-                className="block p-2 border"
-                onClick={() => handleDeleteInputDescription(description.id)}
-              >
-                delete
-              </button>
+          <section className="py-8">
+            <div>
+              <span className="block text-sm mb-2 text-slate-500">Title:</span>
+              <input
+                type="text"
+                value={data?.title}
+                className="w-full block border border-slate-300 rounded px-3 py-2 focus:border-slate-500 focus:outline-0 transition-all duration-300"
+                onChange={(e) => setData({ ...data, title: e.target.value })}
+              />
             </div>
-          ))
+
+            <div>
+              <h3 className="text-slate-600 mt-4">Description:</h3>
+
+              {descriptionArray.length === 0 ? (
+                <span className="block py-4 text-slate-900">
+                  No description available
+                </span>
+              ) : (
+                descriptionArray.map((description: any, index: number) => (
+                  <div key={description.id} className="flex py-4">
+                    <textarea
+                      className="w-full block border border-slate-300 rounded px-3 py-2 focus:border-slate-500 focus:outline-0 transition-all duration-300 resize-none h-32"
+                      value={description.text}
+                      onChange={(e) => {
+                        const text = e.target.value;
+                        setDescriptionArray((currentDescription: any) =>
+                          produce(currentDescription, (v: any) => {
+                            v[index].text = text;
+                          })
+                        );
+                      }}
+                    ></textarea>
+                    <button
+                      className="block p-2 text-2xl px-5 text-red-500 bg-slate-100 ml-4 rounded"
+                      onClick={() =>
+                        handleDeleteInputDescription(description.id)
+                      }
+                    >
+                      <BsTrash />
+                    </button>
+                  </div>
+                ))
+              )}
+            </div>
+
+            <Modal showModal={showModal}>
+              <div className="flex flex-col justify-center h-full items-center">
+                <h1>¿Desea eliminar?</h1>
+
+                <div className="grid grid-cols-2 gap-x-4">
+                  <button
+                    className="block p-2 border"
+                    onClick={() => {
+                      handleDeleteReviewItem(itemDelete);
+                      setShowModal(false);
+                    }}
+                  >
+                    Si
+                  </button>
+                  <button
+                    className="block p-2 border"
+                    onClick={() => {
+                      setShowModal(false);
+                      setItemDelete("");
+                    }}
+                  >
+                    No
+                  </button>
+                </div>
+              </div>
+            </Modal>
+
+            <button
+              className="border border-slate-300 rounded block px-3 py-2 text-slate-500 mb-8"
+              onClick={handleAddInputDescription}
+            >
+              Add input description
+            </button>
+
+            <h1 className="text-2xl text-slate-600 mt-8 mb-6">
+              List of Reviews
+            </h1>
+
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6">
+              {dataItems.map((item: any) => (
+                <div
+                  key={item.id}
+                  className="shadow-lg rounded border border-slate-200 px-4 py-3 h-36 flex flex-col justify-between"
+                >
+                  <span className="block mb-2 text-slate-600">{item.name}</span>
+
+                  <div className="grid grid-cols-2 gap-x-3">
+                    <button
+                      className="block w-full border border-slate-400 py-2 rounded"
+                      onClick={() => router.push(`/admin/review/${item.id}`)}
+                    >
+                      Editar
+                    </button>
+                    <button
+                      className="block w-full bg-slate-500 py-2 rounded text-white"
+                      onClick={() => {
+                        setShowModal(true);
+                        setItemDelete(item.id);
+                      }}
+                    >
+                      Eliminar
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <button
+              className="border border-slate-300 rounded block px-3 py-2 text-slate-500 mt-6"
+              onClick={() => router.push("/admin/create/review")}
+            >
+              Add a new review
+            </button>
+
+            <div className="border border-slate-200 mt-8"></div>
+
+            <button
+              className="bg-slate-700 text-white rounded block px-8 text-lg py-2 mt-8"
+              onClick={handleUpdate}
+            >
+              Update
+            </button>
+
+            {error.status && <span className="block">{error.type}</span>}
+          </section>
         )}
       </div>
-
-      <Modal showModal={showModal}>
-        <div className="flex flex-col justify-center h-full items-center">
-          <h1>¿Desea eliminar?</h1>
-
-          <div className="grid grid-cols-2 gap-x-4">
-            <button
-              className="block p-2 border"
-              onClick={() => {
-                handleDeleteReviewItem(itemDelete);
-                setShowModal(false);
-              }}
-            >
-              Si
-            </button>
-            <button
-              className="block p-2 border"
-              onClick={() => {
-                setShowModal(false);
-                setItemDelete("");
-              }}
-            >
-              No
-            </button>
-          </div>
-        </div>
-      </Modal>
-
-      <button
-        className="block border p-2 mb-3"
-        onClick={handleAddInputDescription}
-      >
-        agregar campo
-      </button>
-      <button className="border block p-2" onClick={handleUpdate}>
-        actualizar
-      </button>
-
-      {error.status && <span className="block">{error.type}</span>}
-
-      <div className="grid grid-cols-3 gap-x-10 mt-8">
-        {dataItems.map((item: any) => (
-          <div key={item.id} className="border p-3">
-            <span className="block">{item.name}</span>
-
-            <button
-              className="block border p-2 mt-4"
-              onClick={() => router.push(`/admin/review/${item.id}`)}
-            >
-              Editar
-            </button>
-            <button
-              className="block border p-2 mt-4"
-              onClick={() => {
-                setShowModal(true);
-                setItemDelete(item.id);
-              }}
-            >
-              Eliminar
-            </button>
-          </div>
-        ))}
-      </div>
-
-      <button
-        className="border p-2 mt-8"
-        onClick={() => router.push("/admin/create/review")}
-      >
-        agregar nueva review
-      </button>
     </div>
   );
 };
