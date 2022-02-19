c9816f71b3e4cf1c9717586e6eede8369483bb1c
fervillalbag <21fervillalbag@gmail.com>
2022-02-19 11:12:58 -0300

    feat: table order item ui

--- app/src/pages/admin/product/orders/[id].tsx
+++ app/src/pages/admin/product/orders/[id].tsx
@@ -2,7 +2,11 @@ import { GET_ORDER } from "@/graphql/queries/orders";
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import React from "react";
-import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import {
+  BsFillArrowLeftCircleFill,
+  // BsToggleOff,
+  // BsToggleOn,
+} from "react-icons/bs";
 
 const OrderItem: React.FC = () => {
   const router = useRouter();
@@ -12,10 +16,6 @@ const OrderItem: React.FC = () => {
       id: router?.query?.id,
     },
   });
-
-  if (loading) return null;
-
-  console.log(order?.getListProduct);
 
   return (
     <div className="flex">
@@ -31,7 +31,99 @@ const OrderItem: React.FC = () => {
         </button>
       </div>
 
-      <div className="p-10 w-full h-screen overflow-y-auto no-scrollbar"></div>
+      <div className="p-10 w-full h-screen overflow-y-auto no-scrollbar">
+        {loading ? (
+          <span className="block">Cargando..</span>
+        ) : (
+          <div className="grid grid-cols-3 gap-x-10">
+            <div className="col-start-1 col-end-3">
+              <div className="grid grid-cols-[70px_1fr_4fr_1fr_1fr] border-b border-slate-200 pb-2 gap-x-8">
+                <div>
+                  <span className="block font-semibold text-slate-600 text-center">
+                    Image
+                  </span>
+                </div>
+                <div>
+                  <span className="block font-semibold text-slate-600 text-center">
+                    Code
+                  </span>
+                </div>
+                <div>
+                  <span className="block font-semibold text-slate-600">
+                    Description
+                  </span>
+                </div>
+                <div>
+                  <span className="block font-semibold text-slate-600 text-center">
+                    Quantity
+                  </span>
+                </div>
+                <div>
+                  <span className="block font-semibold text-slate-600 text-center">
+                    Price
+                  </span>
+                </div>
+              </div>
+
+              {order?.getListProduct?.products.map((product: any) => (
+                <div
+                  key={product.id}
+                  className="grid grid-cols-[70px_1fr_4fr_1fr_1fr] border-b border-slate-200 py-2 items-center gap-x-8"
+                >
+                  <div className="grid place-items-center">
+                    <img
+                      src={product.image}
+                      className="w-10 h-10 rounded-full"
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <span className="block text-sm text-slate-600 text-center">
+                      {product.code}
+                    </span>
+                  </div>
+                  <div>
+                    <span className="block text-sm text-slate-600">
+                      {product.name}
+                    </span>
+                  </div>
+                  <div>
+                    <span className="block text-sm text-center font-semibold text-slate-400">
+                      {product.qty}
+                    </span>
+                  </div>
+                  <div>
+                    <span className="block text-sm text-center font-semibold text-slate-400">
+                      ${product.price}
+                    </span>
+                  </div>
+                </div>
+              ))}
+
+              <div className="grid grid-cols-[70px_1fr_4fr_1fr_1fr] py-3 gap-x-8">
+                <div>
+                  <span className="block text-center font-semibold uppercase">
+                    Total
+                  </span>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div>
+                  <span className="block text-center font-semibold text-slate-600">
+                    $
+                    {order?.getListProduct?.products.reduce(
+                      (a: any, b: any) => a.price + b.price
+                    )}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-slate-200"></div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
