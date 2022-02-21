3e2ddc7bc53a0ec59ddea908242bff158549bf75
fervillalbag <21fervillalbag@gmail.com>
2022-02-21 16:38:01 -0300

    feat: qty action delete and add at cart page

--- app/src/pages/cart.tsx
+++ app/src/pages/cart.tsx
@@ -3,13 +3,13 @@ import Layout from "@/layout";
 import { useCart } from "@/hooks/useCart";
 import { getToken } from "utils/helpers";
 import { isAuth, isUserNotFound } from "utils/actions";
+import { FaMinus, FaPlus } from "react-icons/fa";
 // import useAuth from "@/hooks/useAuth";
 
 const Cart = () => {
   isUserNotFound();
 
-  const { cart } = useCart();
-  // const { user } = useAuth();
+  const { cart, handleAddCart, handleDeleteCart } = useCart();
 
   console.log(cart);
 
@@ -25,9 +25,9 @@ const Cart = () => {
 
   return (
     <Layout>
-      <div className="max-w-2xl mx-auto w-11/12">
+      <div className="max-w-3xl mx-auto w-11/12">
         {cart.length !== 0 && (
-          <div className="grid grid-cols-[70px_1fr_80px_80px] gap-x-6 mb-4 mt-10">
+          <div className="grid grid-cols-[70px_1fr_100px_80px] gap-x-6 mb-4 mt-10">
             <span className="font-semibold text-slate-700 block text-center"></span>
             <span className="font-semibold text-slate-700 block">Name</span>
             <span className="font-semibold text-slate-700 block text-center">
@@ -45,7 +45,7 @@ const Cart = () => {
           cart.map((product: any) => (
             <div
               key={product.id}
-              className="grid grid-cols-[70px_1fr_80px_80px] gap-x-6 mb-6 items-center border-b border-slate-200 pb-6"
+              className="grid grid-cols-[70px_1fr_100px_80px] gap-x-6 mb-6 items-center border-b border-slate-200 pb-6"
             >
               <div>
                 <img
@@ -62,11 +62,25 @@ const Cart = () => {
                   {product.code}
                 </span>
               </div>
-              <div>
-                <span className="block text-slate-400 font-semibold text-center">
+
+              <div className="mt-2 md:mt-0 flex item-center">
+                <button
+                  className="flex items-center justify-center text-xs w-10 h-8 rounded bg-BrightRed text-white"
+                  onClick={() => handleDeleteCart(product)}
+                >
+                  <FaMinus />
+                </button>
+                <span className="text-base md:text-sm block my-1 text-center px-4 md:px-0 font-semibold text-slate-600 mx-3">
                   {product.qty}
                 </span>
+                <button
+                  className="flex items-center justify-center text-xs w-10 h-8 rounded bg-BrightRed text-white"
+                  onClick={() => handleAddCart(product)}
+                >
+                  <FaPlus />
+                </button>
               </div>
+
               <div>
                 <span className="block text-slate-400 font-semibold text-center">
                   ${product.price}
@@ -77,14 +91,19 @@ const Cart = () => {
         )}
 
         {cart.length !== 0 && (
-          <div className="grid grid-cols-[70px_1fr_80px_80px] gap-x-6 items-center pb-56">
+          <div className="grid grid-cols-[70px_1fr_100px_80px] gap-x-6 items-center pb-56">
             <span className="block text-xl font-semibold text-slate-700 uppercase">
               Total
             </span>
             <div></div>
             <div></div>
             <span className="block text-lg font-semibold text-slate-700 text-center">
-              $763
+              $
+              {cart.length === 1
+                ? cart.map((a: any) => a.price * a.qty)
+                : cart.reduce(
+                    (a: any, b: any) => a.price * a.qty + b.price * b.qty
+                  )}
             </span>
           </div>
         )}
