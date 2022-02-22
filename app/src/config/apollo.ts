bd954a9f2bf6fb321c2e28400ff1f3ab48970f20
fervillalbag <21fervillalbag@gmail.com>
2022-02-22 11:38:42 -0300

    change: config apollo connect

--- app/src/config/apollo.ts
+++ app/src/config/apollo.ts
@@ -1,27 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { createUploadLink } from "apollo-upload-client";
-import { setContext } from "@apollo/client/link/context";
-
-import { getToken } from "utils/helpers";
-
-const httpLink = createUploadLink({
-  uri: process.env.URL_API,
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = getToken();
-
-  return {
-    headers: {
-      ...headers,
-      Authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
 
 const client = new ApolloClient({
-  connectToDevTools: true,
-  link: authLink.concat(httpLink),
+  uri: process.env.URL_API,
   cache: new InMemoryCache(),
 });
 
